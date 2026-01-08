79a766fc273a350e0ceb90f55e542d0078c1120f
jimin <jimin4770@naver.com>
2026-01-08 13:25:56 +0900

    Update OG imgage size 1200x630

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,8 +34,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://maedal.com/runner_medal_.png",
-        width: 1024,
-        height: 1024,
+        width: 1200,
+        height: 630,
         alt: "Maedal",
       },
     ],
